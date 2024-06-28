a554c95d9616ec0917f2f898617f29e543d53aa2
Rafael <rafaelpmeirelles@gmail.com>
2024-06-28 15:54:59 -0300

    Testes de gradiente

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ def f1(x):
 
 
 def f_r_2(x):
-    return 100*((x[1]-x[0] ** 2)**2) + (x[0] -1)**2
+    return 100*((x[1]-x[0] ** 2)**2) + (x[0] - 1)**2
 
 
-inputs = [[0, 0], [2, 2], [0, 1], [1, 0], [20, -5]]
+def f_r_3(x):
+    return 100 * (x[1] - x[0]**2)**2 + (x[0] - 1)**2 + 100 * (x[2] - x[1]**2)**2 + (x[1] - 1)**2
 
-print("Testando gradientes")
-for inpt in inputs:
-    print(f"Gradient for f1 in {inpt} = {[round(x, 4) for x in grad.gradient(f1, inpt)]}")
+
+inputs = [[0, 0], [2, 2], [3, 13], [1, 0], [20, -5]]
+functions_2d = [("f1", f1), ("2D Rosenbrock function", f_r_2)]
+
+print("Testing gradients for 2D functions")
+for func_name, func in functions_2d:
+    for inpt in inputs:
+        print(f"Gradient for {func_name} in {inpt} = {[round(x, 4) for x in grad.gradient(func, inpt)]}")
+    print()
+
+inputs = [[0, 0, 0], [1, 2, 3], [1, -2, 4], [-5, -5, -5]]
+functions_3d = [("3D Rosenbrock function", f_r_3)]
+
+print("Testing gradients for 3D functions")
+for func_name, func in functions_3d:
+    for inpt in inputs:
+        print(f"Gradient for {func_name} in {inpt} = {[round(x, 4) for x in grad.gradient(func, inpt)]}")
+    print()
